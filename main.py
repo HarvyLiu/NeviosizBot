d3039ecb30104b82bd2b3c61342aa417f3755d1d
Harvy <harvyliu2009@gmail.com>
2025-11-01 02:17:42 +0800

    Add on_ready event handler to Discord bot

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ load_dotenv() #reads .env file
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 token = os.getenv('TOKEN')
+
+@client.event
+async def on_ready():
+    print(f"Logged in as a bot {client.user}")
+
+
