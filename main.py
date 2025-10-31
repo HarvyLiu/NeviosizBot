2bab312c00abca25ab61b6cbbc83511ef2732f7c
Harvy <harvyliu2009@gmail.com>
2025-11-01 01:49:41 +0800

    intents...

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import discord
 import os
 import random
 from dotenv import load_dotenv
+
+load_dotenv() #reads .env file
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+token = os.getenv('TOKEN')
