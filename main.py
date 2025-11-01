c9b713c57d3a4cb556a627c96dd22a58f77c7199
Harvy <harvyliu2009@gmail.com>
2025-11-01 16:35:35 +0800

    added some more functions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import asyncio
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv() #reads .env file
 intents = discord.Intents.all()
@@ -12,12 +13,14 @@ token = os.getenv('TOKEN')
 @client.event
 async def on_ready():
     status_text = "銀月別再gay了"
+    channel = client.get_channel(1434044899325513738)
     await client.change_presence(
         status=discord.Status.online,
         activity=discord.Game(status_text)
         )
     print(f"Logged in as a bot {client.user}")
     print(f"Status text set to {status_text}")
+    await channel.send(f"Bot {client.user} is online! :white_check_mark:")
 
 @client.event
 async def on_message(message):
@@ -35,9 +38,28 @@ async def on_message(message):
     if uName == "ilovevtuber14." and "寶貝" in uMessage.lower():
         await message.channel.send(f"好了銀月別再gay了")
     if "!dice" in uMessage.lower():
-        await message.channel.send(f"Rolling Dice")
-        dice_sides = uMessage.lower().split()[2]
+        dice_sides = uMessage.lower().split()[1]
+        await message.channel.send(f"Rolling Dice of {dice_sides}......")
         await asyncio.sleep(1)
-        await message.channel.send(f"You rolled: {random.randint(1, int(dice_sides))}")
+        try:
+            await message.channel.send(f"You rolled: {random.randint(1, int(dice_sides))}")
+        except:
+            await message.channel.send(f"Wait... TF is {dice_sides}?\nI suppose you didn't enter a valid number or format?\nThe format should be: !dice [number]")
+    if "!echo" in uMessage.lower():
+        echo_text = uMessage.split()[1]
+        await message.delete()
+        await message.channel.send(echo_text)
+    
+
+@client.event
+async def on_message_delete(message):
+    channel = discord.utils.get(message.guild.channels, name="mod-logs")
+    await channel.send(f"<:alastor_smile:1433856260377411645> Deleted<:delete295:1434047047073533972> Message by {message.author.mention} deleted in {message.channel.mention}: ```{message.content}```")
+
+
+
+
+
 
 client.run(token)
+
